7e81c97bc090683f7f0ad42d234c8786132de329
Theouty <aman.david.michael@gmail.com>
2020-04-04 21:03:35 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,108 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do 
+  end  
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do 
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes' do 
+  @recipes = Recipe.all  
+  erb :index
+  end
+
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end 
+
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    binding.pry
+    @recipe.update({name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]}) #cannot just do @recipe.update(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect '/recipes'
+    end
+
+
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   get '/' do
+#   end
+
+#   get '/recipes/new' do
+#     erb :new
+#   end
+
+#   post '/recipes' do
+#     @recipe = Recipe.create(params)
+#     redirect "/recipes/#{@recipe.id}"
+#   end
+
+#   get '/recipes' do
+#     @recipes = Recipe.all
+#     erb :index
+#   end
+
+#   get '/recipes/:id' do
+#     @recipe = Recipe.find(params[:id])
+#     erb :show
+#   end
+
+#   get '/recipes/:id/edit' do
+#     @recipe = Recipe.find(params[:id])
+#     erb :edit
+#   end
+
+#   patch '/recipes/:id' do
+#     @recipe = Recipe.find(params[:id])
+#     @recipe.update({name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]})
+#     redirect "/recipes/#{@recipe.id}"
+#   end
+
+#   delete '/recipes/:id' do
+#     @recipe = Recipe.find(params[:id])
+#     @recipe.delete
+#     redirect '/recipes'
+#   end
+
+# end
